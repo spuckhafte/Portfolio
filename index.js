a7cd1120541575bbd6776597aae2a56272f52c94
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2022-01-15 16:20:45 +0530

    initialised 'about me', still buggy

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let welcome_text_elem = document.getElementById('welcome-message')
+let about_me_elem = document.getElementById('about-me')
+let welcome_message_complete = false
 
 window.onload = () => {
-    let welcome_text = 'Hi, this is Rakshit                     '
+    let welcome_text = '~Hi, this is Rakshit                     '
     let i = 0
     setTimeout(() => {
         const write_message = setInterval(() => {
@@ -10,6 +12,7 @@ window.onload = () => {
             if (i == welcome_text.length) {
                 welcome_text_elem.innerHTML = ''
                 i = 0;
+                if (about_me_elem.style.display === 'none') about_me_elem.style.display = 'block'
             }
         }, 100);
     }, 500)
