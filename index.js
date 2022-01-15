6e4035baa859dc19ccb7b140fb34a586b9b9b981
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2022-01-15 16:06:04 +0530

    Welcome message

---
+++ index.js
@@ -0,0 +1,16 @@
+let welcome_text_elem = document.getElementById('welcome-message')
+
+window.onload = () => {
+    let welcome_text = 'Hi, this is Rakshit                     '
+    let i = 0
+    setTimeout(() => {
+        const write_message = setInterval(() => {
+            welcome_text_elem.innerHTML += welcome_text.charAt(i)
+            i++;
+            if (i == welcome_text.length) {
+                welcome_text_elem.innerHTML = ''
+                i = 0;
+            }
+        }, 100);
+    }, 500)
+}
