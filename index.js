e5f6ab565990bfea597aafb97543181b393ab187
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2022-01-15 16:22:37 +0530

    minor update (very minor)

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ let about_me_elem = document.getElementById('about-me')
 let welcome_message_complete = false
 
 window.onload = () => {
-    let welcome_text = '~Hi, this is Rakshit                     '
+    let welcome_text = 'Hi, this is Rakshit                     '
     let i = 0
     setTimeout(() => {
         const write_message = setInterval(() => {
             welcome_text_elem.innerHTML += welcome_text.charAt(i)
             i++;
             if (i == welcome_text.length) {
-                welcome_text_elem.innerHTML = ''
+                welcome_text_elem.innerHTML = '~'
                 i = 0;
                 if (about_me_elem.style.display === 'none') about_me_elem.style.display = 'block'
             }
