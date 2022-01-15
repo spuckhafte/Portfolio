c3a35257634d43a35b00b7c65e3ea9f20bd35b6b
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2022-01-15 23:41:12 +0530

    content addition

--- index.js
+++ index.js
@@ -1,19 +1,67 @@
-let welcome_text_elem = document.getElementById('welcome-message')
-let about_me_elem = document.getElementById('about-me')
-let welcome_message_complete = false
+let main_elem = document.getElementById('main')
+let more = document.getElementById('more')
+let skills = document.getElementById('skills')
+
+let welcome_text_1_elem = document.getElementById('welcome-message-1'); let welcome_text_1 = 'Person:  '
+let welcome_text_2_elem = document.getElementById('welcome-message-2'); let welcome_text_2 = '⠀⠀initialize(self):'
+let welcome_text_3_elem = document.getElementById('welcome-message-3'); let welcome_text_3 = '⠀⠀⠀⠀self.name = Rakshit'
+let welcome_text_4_elem = document.getElementById('welcome-message-4'); let welcome_text_4 = '⠀⠀⠀⠀self.age = 17'
 
 window.onload = () => {
-    let welcome_text = 'Hi, this is Rakshit                     '
-    let i = 0
     setTimeout(() => {
-        const write_message = setInterval(() => {
-            welcome_text_elem.innerHTML += welcome_text.charAt(i)
-            i++;
-            if (i == welcome_text.length) {
-                welcome_text_elem.innerHTML = '~'
-                i = 0;
-                if (about_me_elem.style.display === 'none') about_me_elem.style.display = 'block'
-            }
-        }, 100);
+        runWelcome()
     }, 500)
+}
+
+function runWelcome() {
+    var count;
+    count += typing_animation(welcome_text_1_elem, welcome_text_1, 50, false, more, 'welcomeAnimation')
+    count += typing_animation(welcome_text_2_elem, welcome_text_2, 50, false, more)
+    count += typing_animation(welcome_text_3_elem, welcome_text_3, 50, false, more)
+    count += typing_animation(welcome_text_4_elem, welcome_text_4, 50, true, more)
+
+    return count;
+}
+
+function typing_animation(elem, text, interval, end, input, type) {
+    let i = 0;
+    if (type == 'skillsAnimation') elem.innerHTML = ''
+    const write_message = setInterval(() => {
+        elem.innerHTML += text.charAt(i)
+        i++;
+        if (i == text.length) {
+            clearTimeout(write_message)
+            if (end) {
+                if (input.style.opacity == 0) {
+                    input.style.opacity = '1';
+                    input.style.transition = '0.2s ease-in'
+                }
+            }
+            return 1;
+        }
+    }, interval);
+}
+
+more.onclick = function () {
+    if (more.innerHTML == './more') {
+        more.innerHTML = './less'
+        main_elem.style.opacity = 1;
+        main_elem.style.transition = '0.1s ease-in'
+        animate_skill()
+    } else {
+        more.innerHTML = './more'
+        main_elem.style.opacity = 0;
+        main_elem.style.transition = '0.1s ease-in'
+    }
+}
+
+function animate_skill() {
+    const skills = [15, 14, 12, 11, 10]
+
+    const langs = document.getElementsByClassName('box-progress')
+    var i
+    for (i = 0; i < langs.length; i++) {
+        let hashtags = '#'.repeat(skills[i])
+        typing_animation(langs[i], hashtags, 20, false, langs[i], 'skillsAnimation')
+    }
 }
